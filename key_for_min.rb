69b51e82ffb7a7d21731b9c4cf9f38a113b4a95d
Austin Powell <austinpowell58@gmail.com>
2018-08-12 18:18:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,9 @@
 def key_for_min_value(name_hash)
   correct_key = nil
   name_hash.each do  |key, value|
-    if value <= 
+    if value <= prev_value
+      prev_value = value
+      correct_key = key
 
   correct_key
 end
